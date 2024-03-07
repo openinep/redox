9ec23d2476722cc1ea1a3b590ebdaa1f58acb46c
alikindsys <alice@blocovermelho.org>
2024-03-07 13:39:17 -0300

    [ormolu] format Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,30 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
-import System.Environment (getArgs)
+
 import System.Console.CmdArgs
+import System.Environment (getArgs)
 
-
-data Cor = Azul | Amarelo | Branco | Rosa | Laranja 
-           deriving (Show, Data, Typeable, Enum, Bounded)
+data Cor = Azul | Amarelo | Branco | Rosa | Laranja
+  deriving (Show, Data, Typeable, Enum, Bounded)
 
 instance Default Cor where
-     def = Azul
-
+  def = Azul
 
 data Interface = Interface {ano :: Int, cor :: Cor, gabarito :: Bool, pdf :: FilePath}
-                 deriving (Show, Data, Typeable)
+  deriving (Show, Data, Typeable)
 
-interface = Interface { ano = def &= help "O ano da prova"
-                      , cor = def &= help "A cor da prova"
-                      , gabarito = def &= help "Se é um gabarito"
-                      , pdf = def &= args &= typ "Arquivo PDF" 
-                      } &= program "redox" &= details ["Exemplos:", "redox -a 2023 -c branco -g <pdf> : GABARITO da prova BRANCO de 2023", "redox -a 2023 -c azul <pdf> : PROVA AZUL de 2023"]
+interface =
+  Interface
+    { ano = def &= help "O ano da prova",
+      cor = def &= help "A cor da prova",
+      gabarito = def &= help "Se é um gabarito",
+      pdf = def &= args &= typ "Arquivo PDF"
+    }
+    &= program "redox"
+    &= details ["Exemplos:", "redox -a 2023 -c branco -g <pdf> : GABARITO da prova BRANCO de 2023", "redox -a 2023 -c azul <pdf> : PROVA AZUL de 2023"]
 
 main :: IO ()
 main = do
-    args <- getArgs
-    print =<< cmdArgs interface
+  args <- getArgs
+  print =<< cmdArgs interface
